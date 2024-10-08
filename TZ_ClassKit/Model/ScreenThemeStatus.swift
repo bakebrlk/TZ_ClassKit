710f255a490284502992ef39bdb796ca330ae036
bakebrlk <210103194@stu.sdu.edu.kz>
2024-10-08 18:05:28 +0500

    add screen theme status

---
+++ TZ_ClassKit/Model/ScreenThemeStatus.swift
@@ -0,0 +1,13 @@
+//
+//  ScreenThemeStatus.swift
+//  TZ_ClassKit
+//
+//  Created by bakebrlk on 08.10.2024.
+//
+
+import SwiftUI
+
+enum ScreenThemeStatus: Codable{
+    case light
+    case dark
+}
