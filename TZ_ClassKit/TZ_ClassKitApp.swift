3549b40dd4c4755de48e63d21e4bbc3c26df91fa
bakebrlk <210103194@stu.sdu.edu.kz>
2024-10-08 16:22:35 +0500

    Initial Commit

---
+++ TZ_ClassKit/TZ_ClassKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TZ_ClassKitApp.swift
+//  TZ_ClassKit
+//
+//  Created by bakebrlk on 08.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TZ_ClassKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
