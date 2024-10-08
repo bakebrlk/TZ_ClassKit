e4d62e12935f0810848a40b620b30e454a3d157a
bakebrlk <210103194@stu.sdu.edu.kz>
2024-10-08 18:47:17 +0500

    fix bug

--- TZ_ClassKit/ModelView/CounterVM.swift
+++ TZ_ClassKit/ModelView/CounterVM.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 final class CounterVM: ObservableObject {
-    @StateObject public static var shared: CounterVM = CounterVM()
+    @ObservedObject public static var shared: CounterVM = CounterVM()
     
     @Published public var count: CGFloat = 0
     @Published public var themeStatus: ScreenThemeStatus = .dark
